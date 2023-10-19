f02db25144c2963fccdeb81f67807ffb57cad3c2
pankaj-gitcode <pankajaditya75@gmail.com>
2023-10-19 17:45:17 +0530

    react-portfolio-v1

--- React_Projects/react_Router_Project/code/reactrouterportfolio/src/main.jsx
+++ React_Projects/react_Router_Project/code/reactrouterportfolio/src/main.jsx
@@ -5,18 +5,23 @@ import './index.css'
 import './component/CSS/Footer.css'
 import './component/CSS/Home.css'
 import './component/CSS/About.css'
+import './component/CSS/Contact.css'
 
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './component/Home/Home.jsx'
 import About from './component/About/About.jsx'
+import Contact from './component/Contact/Contact.jsx'
+import User from './User/User.jsx'
 
 //create a router
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
         <Route path="" element={<Home />} />
-        <Route path="/about" element={<About/>}/>
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="user/:userid" element={<User />}/>
     </Route>
   )
 )
