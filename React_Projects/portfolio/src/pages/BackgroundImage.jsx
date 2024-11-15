db63761f04816473d2d921765ca9c938097c5429
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-15 21:09:47 +0530

    v0

---
+++ React_Projects/portfolio/src/pages/BackgroundImage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BackgroundImage(){
+    return(<div>
+        <div className="h-[80vh] w-[50vw] bg-[url('/public/images/user_me_2.jpeg')]
+         bg-cover bg-center -drop-shadow-[2px_2px_8px_6px_rgba(255,255,255,0.5)] ">
+            img
+        </div>
+    </div>)
+}
