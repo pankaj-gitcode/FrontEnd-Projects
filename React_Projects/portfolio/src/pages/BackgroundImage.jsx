364177b6f97b30f2e1904e9e134c43847bf1a236
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-15 23:15:33 +0530

    v0.1

--- React_Projects/portfolio/src/pages/BackgroundImage.jsx
+++ React_Projects/portfolio/src/pages/BackgroundImage.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 export default function BackgroundImage(){
     return(<div>
-        <div className="h-[80vh] w-[50vw] bg-[url('/public/images/user_me_2.jpeg')]
-         bg-cover bg-center -drop-shadow-[2px_2px_8px_6px_rgba(255,255,255,0.5)] ">
-            img
+        <div className="h-[80vh] w-[50vw] drop-shadow-[10px_2px_90px_rgba(100,200,100,0.8)] ">
+            <img src="/public/images/me1.png" alt="" className='shadow-sm shadow-gray-500'/>
+        </div>
+
+        <div className="bg-black h-screen flex items-center justify-center">
+            <img
+                src="/public/images/me1.png"
+                alt="User"
+                className="shadow-lg shadow-gray-500 rounded-lg"
+            />
         </div>
     </div>)
 }
