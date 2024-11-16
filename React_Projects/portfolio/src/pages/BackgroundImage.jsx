3c4ffb3acec0db2e3c94f966db2ce1f0da914ec2
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-16 14:12:14 +0530

    v0.3

--- React_Projects/portfolio/src/pages/BackgroundImage.jsx
+++ React_Projects/portfolio/src/pages/BackgroundImage.jsx
@@ -8,11 +8,13 @@ export default function BackgroundImage(){
            bg-right-top 
          ">
         </div> */}
-        <div className='bg-[#333232] h-screen'
+        {/* <div className='bg-[#333232] h-screen' */}
+        <div className='h-screen bg-gradient-to-l hover:bg-gradient-to-tr from-slate-100 to-slate-600'
         >
            <img src="/public/images/me2.png" alt="" 
-           className='absolute right-20 top-20 drop-shadow-[10px_20px_500px_rgba(255,255,255,0.6)]
-            h-[80vh] rounded-b-full border-b-2 border-opacity-5 border-[#d4c9c9]            
+           className='absolute right-20 top-20 drop-shadow-[10px_20px_300px_rgba(255,255,255,10)]
+            h-[200vh] lg:h-[90vh] rounded-b-full border-b-2 border-opacity-5 border-[#d4c9c9] 
+                     
         '/>
 
         </div>
