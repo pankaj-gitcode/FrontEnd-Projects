2df614e373e656388d1e76c2ab635b8635931b09
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-18 20:47:18 +0530

    1st commit

---
+++ React_Projects/miniEffects/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import {CustomMouse} from './components/CustomMouse'
+import CustomCursor from './components/CustomCursor'
+
+export default function App() {
+  return (
+    <div>
+      
+      
+      {/* <CustomMouse /> */}
+      <CustomCursor />
+
+    </div>
+  )
+}
---
+++ React_Projects/miniEffects/src/components/CustomCursor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CustomCursor(){
+    return(<>
+        
+        <div className='bg-red-600 text-8xl  h-20 w-20'>
+            HEll0 There
+        </div>
+                
+    </>)
+}
