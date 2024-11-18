0f652accff87a51e36dc2b0016aebd717df0d0cd
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-18 11:01:29 +0530

    short info v0

--- React_Projects/portfolio/src/assets/components/ShortInfo.jsx
+++ React_Projects/portfolio/src/assets/components/ShortInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ShortInfo(){
     return(<>
         <div className=' absolute z-20 flex flex-col items-center justify-center
-        lg:top-64 top-32 sm:top-36 left-12 sm:left-20 lg:text-5xl text-2xl sm:text-5xl
+        lg:top-64 top-32 sm:top-36 left-12 sm:left-20 lg:text-5xl text-4xl sm:text-6xl
         '>
             <h1 className='font-[font4] lg:text-[5vw]'>
                 I AM
@@ -11,8 +11,7 @@ export default function ShortInfo(){
                 <span className='font-[font4] lg:text-7xl lg:text-[7vw]'><br/>DEVELOPER</span>
                 <span className=''><br/>TO HIRE</span>
             </h1>
-            {/* <h1 className='bg-green-500 lg:text-7xl text-start font-[font4]'>DEVELOPER</h1>
-            <h1 className='bg-yellow-500'>TO HIRE</h1> */}
+            
         </div>
     </>)
 }
