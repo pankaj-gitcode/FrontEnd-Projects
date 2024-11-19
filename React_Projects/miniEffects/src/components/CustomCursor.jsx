4248635b11cb4e09cfbff1033f6837eb7f0fb073
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-20 01:10:35 +0530

    code updated animation works

--- React_Projects/miniEffects/src/components/CustomCursor.jsx
+++ React_Projects/miniEffects/src/components/CustomCursor.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export default function CustomCursor(){
+    const msRef = useRef();
+    const mouseMove = (e)=>{
+        msRef.current.style.transform = `rotate(${e.clientX}deg)`
+    }
     return(<>
         {/* --------- container --------- */}
-        <div className='h-screen flex items-center justify-center '>
+        <div onMouseMove={(e)=>{mouseMove(e)}}
+        className='h-screen flex items-center justify-center '>
             
             {/* ------------ child ----------- */}
             
-            <div className='bg-slate-500'>
+            <div ref={msRef} className='bg-slate-500'>
                 <h1 className='text-8xl'>Hello i'm Frontend <br/>Developer </h1>
             </div>
         </div>
