29ad00163eb8fee726715846de56ba6833b79823
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-20 01:06:31 +0530

    recreated

--- React_Projects/miniEffects/src/components/CustomCursor.jsx
+++ React_Projects/miniEffects/src/components/CustomCursor.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function CustomCursor(){
-    const [pos, setPos] = useState([{x:0}])
-    const mouseMove = (e)=>{
-        setPos(prev=>({
-            prev,x:[e.clientX,e.clientY]
-        }))
-        console.log(pos.x)
-
-    }
     return(<>
-        <div className='h-screen' onMouseMove={mouseMove}>
-            <div className='h-10 w-10 bg-red-500 rounded-full '></div>
-            <p>hi{pos.x}, {pos.x}</p>
+        {/* --------- container --------- */}
+        <div className='h-screen flex items-center justify-center '>
+            
+            {/* ------------ child ----------- */}
+            
+            <div className='bg-slate-500'>
+                <h1 className='text-8xl'>Hello i'm Frontend <br/>Developer </h1>
+            </div>
         </div>
     </>)
 }
