c20b3ad1fb25a2212637fd823d362a68778e46d2
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-21 23:23:26 +0530

    child div cordinates update v1

--- React_Projects/miniEffects/src/components/CustomCursor.jsx
+++ React_Projects/miniEffects/src/components/CustomCursor.jsx
@@ -6,26 +6,29 @@ export default function CustomCursor(){
     const mouseMove = (e)=>{
         
         // (0,0) starts from left top corner in main screen, need to start @ middle of child div
-        const xCord = 
-
+       
+        const xCord =  -(e.clientX - msRef.current.getBoundingClientRect().x - msRef.current.getBoundingClientRect().width/2)/20;
+        const yCord = -(e.clientY - msRef.current.getBoundingClientRect().y - msRef.current.getBoundingClientRect().height/2)/20;
+        // console.log('x.y_cord: ', [xCord, yCord]);
         setPos(prev=>({
             ...prev, 
-            X:1,
-            Y:1
+            X:xCord,
+            Y:yCord
         }))
-        msRef.current.style.transform = `rotate(${pos.X, pos.Y}deg)`
+        msRef.current.style.transform = `rotateX(${pos.X}deg) rotateY(${pos.Y}deg)`
         // msRef.current.style.transform = `rotate(${e.clientX, e.clientY}deg) `
 
     }
     return(<>
         {/* --------- container --------- */}
-        <div onMouseMove={(e)=>{mouseMove(e)}}
-        className='h-screen flex items-center justify-center '>
+        <div  onMouseMove={(e)=>{mouseMove(e)}}
+        className='h-screen flex items-center justify-center  '>
             
             {/* ------------ child ----------- */}
             
-            <div ref={msRef} className='bg-slate-500'>
-                <h1 className='text-8xl'>Hello i'm Frontend <br/>Developer </h1>
+            <div id='cont' ref={msRef} className='bg-gray-500'>
+                <h1 className='text-6xl'>Hello i'm Frontend <br/>Developer </h1>
+                <h1 className='text-6xl'>To Hire</h1>
             </div>
         </div>
     </>)
