555e7299607a4fab6a85b523f182fd109c5bd427
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-30 22:23:37 +0530

    page2 anumation:v0

--- React_Projects/portfolio/src/assets/components/page2/Pg2Quotes.jsx
+++ React_Projects/portfolio/src/assets/components/page2/Pg2Quotes.jsx
@@ -1,14 +1,32 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React from 'react'
 
 export default function Pg2Quotes(){
+    useGSAP(()=>{
+        gsap.to('#txt-div', {
+            opacity:1,
+            y:0
+        })
+        gsap.fromTo('.txts', {
+            opacity:0,
+            y:20
+        },{
+            opacity:1,
+            y:0,
+            duration:1,
+            stagger:1.2
+        })
+        }, [])
     return(<>
-    <div className=' text-black flex flex-col items-center bg-yellow-500
-    justify-center uppercase font-[font5] text-center leading-[32vw] overflow-none'>
+    <div className=' text-black flex flex-col items-center 
+    justify-center uppercase font-[font5] text-center 
+    leading-[32vw] overflow-none pt-20' id="txt-div">
 
-        <h1 className=' text-[20vw] sm:text-[40vw] leading-none'>build websites</h1>
-        <h1 className=' text-[20vw] sm:text-[40vw] leading-none'>that</h1>
-        <h1 className=' text-[20vw] sm:text-[40vw] leading-none'>create</h1>
-        <h1 className=' text-[20vw] sm:text-[40vw] leading-none'>legacies</h1>
+        <h1 className='txts text-[20vw] sm:text-[40vw] '>build websites</h1>
+        <h1 className='txts text-[20vw] sm:text-[40vw] '>that</h1>
+        <h1 className='txts text-[20vw] sm:text-[40vw] '>create</h1>
+        <h1 className='txts text-[20vw] sm:text-[40vw] '>legacies</h1>
         
     </div>
     </>)
