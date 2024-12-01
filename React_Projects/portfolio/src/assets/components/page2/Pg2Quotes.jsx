96a4b8d9457ce4d6b74e3e8a5a37f312838f127b
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-01 09:01:52 +0530

    page2 animation:v1

--- React_Projects/portfolio/src/assets/components/page2/Pg2Quotes.jsx
+++ React_Projects/portfolio/src/assets/components/page2/Pg2Quotes.jsx
@@ -4,30 +4,27 @@ import React from 'react'
 
 export default function Pg2Quotes(){
     useGSAP(()=>{
-        gsap.to('#txt-div', {
-            opacity:1,
-            y:0
+        gsap.from('.txts', {
+            transform: 'rotateX(-90deg)',
+            stagger:1.2,
+            duration:2,
+            opacity:0
         })
-        gsap.fromTo('.txts', {
-            opacity:0,
-            y:20
-        },{
-            opacity:1,
-            y:0,
-            duration:1,
-            stagger:1.2
-        })
-        }, [])
+    })
     return(<>
-    <div className=' text-black flex flex-col items-center 
-    justify-center uppercase font-[font5] text-center 
-    leading-[32vw] overflow-none pt-20' id="txt-div">
+    <div className='page2'>
 
-        <h1 className='txts text-[20vw] sm:text-[40vw] '>build websites</h1>
-        <h1 className='txts text-[20vw] sm:text-[40vw] '>that</h1>
-        <h1 className='txts text-[20vw] sm:text-[40vw] '>create</h1>
-        <h1 className='txts text-[20vw] sm:text-[40vw] '>legacies</h1>
-        
+        <div className=' text-black flex flex-col items-center 
+        justify-center uppercase font-[font5] text-center 
+        leading-[32vw] overflow-none pt-20' id="txt-div">
+
+            <h1 className='txts text-[20vw] sm:text-[40vw] '>build </h1>
+            <h1 className='txts text-[20vw] sm:text-[40vw] '>websites</h1>
+            <h1 className='txts text-[20vw] sm:text-[40vw] '>that</h1>
+            <h1 className='txts text-[20vw] sm:text-[40vw] '>create</h1>
+            <h1 className='txts text-[20vw] sm:text-[40vw] '>legacies</h1>
+            
+        </div>
     </div>
     </>)
 }
