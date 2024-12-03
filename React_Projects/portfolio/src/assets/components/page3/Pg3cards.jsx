edf3cf1d75062a5afe3fe87628908fe0437b4441
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-03 22:39:01 +0530

    page3-card scroller createdv0

--- React_Projects/portfolio/src/assets/components/page3/Pg3cards.jsx
+++ React_Projects/portfolio/src/assets/components/page3/Pg3cards.jsx
@@ -16,7 +16,7 @@ export default function Pg3cards(){
                 scrollTrigger:{
                     trigger:elem,
                     markers:true,
-                    start:'top 15%',
+                    start:'top 10%',
                     end: 'bottom 15%',
                     scrub:true,
                     
@@ -31,23 +31,30 @@ export default function Pg3cards(){
             {/* --------- CARD-1 ---------- */}
             <div className='card sticky top-[5vh] h-[70vh] sm:h-[80vh] lg:h-[90vh] w-[70vw] sm:w-[50vw] lg:w-[30vw] rounded-3xl 
              shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-gray-500 mb-20 
-             bg-gradient-to-right from-gray-500 to-gray-300'></div>
+             bg-gradient-to-bl from-red-500 to-black'></div>
 
             {/* --------- CARD-2 ---------- */}
             <div className='card sticky top-[10vh] h-[70vh] sm:h-[80vh] lg:h-[90vh] w-[70vw] sm:w-[50vw] lg:w-[30vw] rounded-3xl 
-             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-orange-500 mb-20 '></div>
+             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-orange-500 mb-20
+             bg-gradient-to-b from-purple-500 via-pink-500 to-red-500'></div>
 
             {/* --------- CARD-3 ---------- */}
             <div className='card sticky top-[10vh] h-[70vh] sm:h-[80vh] lg:h-[90vh] w-[70vw] sm:w-[50vw] lg:w-[30vw] rounded-3xl 
-             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-red-500 mb-20 '></div>
+             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-red-500 mb-20 
+             bg-gradient-to-tr from-blue-500 to-cyan-400'></div>
 
             {/* --------- CARD-4 ---------- */}
             <div className='card sticky top-[10vh] h-[70vh] sm:h-[80vh] lg:h-[90vh] w-[70vw] sm:w-[50vw] lg:w-[30vw] rounded-3xl 
-             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-green-500 mb-20 '></div>
+             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-green-500 mb-20
+             bg-gradient-to-l from-yellow-400 via-orange-500 to-red-600 '></div>
 
             {/* --------- CARD-5 ---------- */}
             <div className='card sticky top-[10vh] h-[70vh] sm:h-[80vh] lg:h-[90vh] w-[70vw] sm:w-[50vw] lg:w-[30vw] rounded-3xl 
-             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-blue-500 mb-20 '></div>
+             shadow-[2px_2px_8px_6px_rgba(0,0,0,0.8)] bg-blue-500 mb-20 
+             bg-gradient-to-r from-purple-500 via-pink-500 to-red-500
+              text-transparent '>
+                This is gradient from purple500 via <br/> pink500 to red500
+             </div>
 
             {/* --------- CARD-6 ---------- */}
             <div className='card sticky top-[10vh] h-[70vh] sm:h-[80vh] lg:h-[90vh] w-[70vw] sm:w-[50vw] lg:w-[30vw] rounded-3xl 
