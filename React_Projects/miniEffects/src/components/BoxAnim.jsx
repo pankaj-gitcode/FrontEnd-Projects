b0412b2ccfe6a0af5f715a1d4ce35c0fc0b9c2fb
pankaj-gitcode <pankajaditya75@gmail.com>
2024-12-07 22:49:33 +0530

    Animate cursor move v2

--- React_Projects/miniEffects/src/components/BoxAnim.jsx
+++ React_Projects/miniEffects/src/components/BoxAnim.jsx
@@ -4,14 +4,17 @@ import React, { useRef, useState } from 'react'
 
 export default function BoxAnim(){
     const [pos, setPos] = useState({x:0, y:0});
+    gsap.registerPlugin();
     // refs for both the boxes
     const box1Ref = useRef();
     const box2Ref = useRef();
 
     // set the cords:x&y
     const animBox = (e)=>{
-        const xCord = e.clientX;
-        const yCord = e.clientY;
+        const xCord = e.clientX-box1Ref.current.getBoundingClientRect().x-box1Ref.current.getBoundingClientRect().width/2;
+        const yCord = e.clientY-box1Ref.current.getBoundingClientRect().y-box1Ref.current.getBoundingClientRect().height/2;
+
+        console.log('bOundingClient: ', box1Ref.current.getBoundingClientRect())
 
         setPos(prev=>({
             ...prev, x:xCord, y:yCord
@@ -24,13 +27,15 @@ export default function BoxAnim(){
     //animate through GSAP
     useGSAP(()=>{
         gsap.to(box1Ref.current, {
-            transform: `rotateX(${pos.x}deg) rotateY(${pos.y}deg)`,
-            duration:10,
+            transform: `rotateX(${pos.x/2}deg) rotateY(${pos.y/2}deg) `,
+            duration:20,
+            ease:'elastic'
 
         })
         gsap.to(box2Ref.current, {
-            transform: `rotateX(${pos.x}deg) rotateY(${pos.y}deg)`,
-            duration:8,
+            transform: `rotateX(${pos.x+20}deg) rotateY(${pos.y+20}deg)`,
+            duration:50,
+            ease: 'elastic'
         })
     }, [pos.x, pos.y])
 
@@ -42,12 +47,19 @@ export default function BoxAnim(){
             <h1 className='text-6xl text-white bg-black py-10'>This is Box Animation</h1>
 
                         {/* ------ BOXES -------- */}
-            <div className='flex flex-col items-center justify-center 
+            <div className='flex flex-col items-center justify-center gap-10
             relative bg-gray-500 h-96 min-w-[100vw]'
             onMouseMove={(e)=>animBox(e)} >
+                            {/* ------ B1----- */}
+                <div ref={box1Ref} className='bg-orange-500 h-60 w-60 blur-lg rounded-l-[12vw] rounded-r-[12vw]'> 1stDIV</div>
 
-                <div ref={box1Ref} className='bg-orange-500 h-60 w-60'> 1stDIV</div>
-                <div ref={box2Ref} className='bg-blue-500 h-60 w-60 absolute -skew-x-2 '> 2ndDIV</div>
+                            {/* ------ B2 ------ */}
+                <div ref={box2Ref} className='bg-orange-500 h-60 w-60 absolute -skew-x-12
+                 blur-md rounded-none ml-20'> 2ndDIV</div>
+
+                            {/* ----- B3 ------ */}
+                <div className='bg-blue-500 absolute h-52 w-52 rounded-full blur-sm'>3</div>
+                
             </div>
 
         </div>
