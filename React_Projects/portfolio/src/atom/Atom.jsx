b6a9e83d31808bfa50d2cddb02710f804e088bd0
pankaj-gitcode <pankajaditya75@gmail.com>
2025-02-16 02:26:23 +0530

    project skeleton loaded v0

--- React_Projects/portfolio/src/atom/Atom.jsx
+++ React_Projects/portfolio/src/atom/Atom.jsx
@@ -1,9 +1,37 @@
 import { atom } from "recoil";
 
-export const projectTitleAtom = atom({
+export const projectAtom = atom({
     key: 'projectTitleAtom',
     default: [
-        {},
-        {},
-        {}]
+        {
+            projectId: 1,
+            projectTitle: 'Text To Image Generator',
+            gitHubLink: 'https://github.com/pankaj-gitcode1',
+            webLink: 'https://google.com1'
+        },
+        {
+            projectId: 2,
+            projectTitle: 'Food Delivery WebApp',
+            gitHubLink: 'https://github.com/pankaj-gitcode2',
+            webLink: 'https://google.com2'
+        },
+        {
+            projectId: 3,
+            projectTitle: 'Portfolio',
+            gitHubLink: 'https://github.com/pankaj-gitcode3',
+            webLink: 'https://google.com3'
+        },
+        {
+            projectId: 4,
+            projectTitle: 'Password Generator',
+            gitHubLink: 'https://github.com/pankaj-gitcode4',
+            webLink: 'https://google.com4'
+        },
+        {
+            projectId: 5,
+            projectTitle: 'AI Code Editor',
+            gitHubLink: 'https://github.com/pankaj-gitcode5',
+            webLink: 'https://google.com5'
+        }
+    ]
 })
--- React_Projects/portfolio/src/pages/Page3Projects.jsx
+++ React_Projects/portfolio/src/pages/Page3Projects.jsx
@@ -1,7 +1,11 @@
 import Spline from '@splinetool/react-spline'
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import { projectAtom} from '../atom/Atom'
 
 export const Page3Projects = () => {
+    const projectAssets = useRecoilValue(projectAtom);
+    console.log('Project Title: ', projectAssets);
   return (<>
 
     {/* ----- ROBOT + PROJECT DIV ----- */}
@@ -9,11 +13,53 @@ export const Page3Projects = () => {
         <Spline scene="https://prod.spline.design/6ergQDvH14-iwc63/scene.splinecode" />
 
         {/* ----- PROJECT SECTION ----- */}
-        <div>
-            
-        </div>
+        {
+            projectAssets.map((el,i)=>
+            <div key={el.projectId} className='bg-blue-500'>
+                {
+                    <div className={`flex items-center justify-between 
+                    ${el.projectId % 2 === 0? 'flex-row-reverse': '' }`}>
+                        <div>
+                            <h1>{el.projectTitle}</h1>
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <a href="">{el.gitHubLink}</a>
+                            <a href="">{el.webLink}</a>
+                        </div>
+                    </div>
+                }
+            </div>
+            )
+        }
+           
     </div>
 
   </>
   )
 }
+
+
+// {
+//     projectAssets.map((el, i)=>
+// <div className='bg-blue-500 ' key={el.projectId} >{
+//  el.projectId%2===0?
+//         <div>
+//             <div>
+//                 <a href="">{el.gitHubLink}</a>
+//                 <a href="">{el.webLink}</a>
+//             </div>
+//             <h1>{el.projectTitle}</h1>
+//         </div>
+//         :
+//         <div>
+//             <h1>{el.projectTitle}</h1>
+//             <div>
+//                 <a href="">{el.gitHubLink}</a>
+//                 <a href="">{el.webLink}</a>
+//             </div>
+//         </div>
+        
+// }</div>
+//     )
+//    }
