dff26288e0829cc7c67d82d1cdc41922469be0ae
pankaj-gitcode <pankajaditya75@gmail.com>
2025-02-20 01:39:18 +0530

    bug fixed

--- React_Projects/portfolio/src/pages/Page4TextVideo.jsx
+++ React_Projects/portfolio/src/pages/Page4TextVideo.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import { videoAtom } from '../atom/Atom'
 
 export const Page4TextVideo = () => {
+  const video = useRecoilValue(videoAtom);
   return (
-    <div className='h-screen'>
-        <h1 className="text-black min-h-screen bg-red-600  ">
-        Hello this is TEXT4VideoText
-        </h1>
+    <div className=' min-h-screen bg-red-500'>
+
+        {/* <video controls='false' loop='true' preload='metadata' muted='false' */}
+
+        <div className='h-screen bg-yellow-500'>
+        <video loop={true} controls={false} autoPlay muted={true} 
+        className=' bg-green-600' 
+        playsInline>
+          <source src={video} type='video/mp4' / >
+        </video>
+        </div>
+
     </div>
   )
 }
