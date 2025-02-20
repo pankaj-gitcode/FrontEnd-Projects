32d279c23b332548b857be589181d985f1cb1dbe
pankaj-gitcode <pankajaditya75@gmail.com>
2025-02-21 04:31:56 +0530

    contactMe form page v1

--- React_Projects/portfolio/src/pages/ContactForm.jsx
+++ React_Projects/portfolio/src/pages/ContactForm.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Spline from '@splinetool/react-spline';
 
 export const ContactForm = () => {
+  const [zIndex, setZIndex] = useState(false)
   return (<>
-  <div className='h-screen '>
-    <section className='bg-black h-screen mt-5 
+  <div className='h-screen relative '>
+
+      <div className='h-screen'>
+
+
+        {/* ----- BUTTON ----- */}
+      <div 
+      className= {`rounded-full absolute right-7 bottom-5 `}
+      onClick={()=>setZIndex(true)} >
+        
+        <button
+          className='uppercase border-8 rounded-full px-7 py-3
+          shadow-[2px_2px_19px_2px_rgba(255,255,255,0.5)] 
+          active:scale-105 transition-300 ease-in-out border-[#9BFFF7]
+          bg-black text-white'>
+        Click Here     
+        </button>
+      </div>
+
+    <Spline scene="https://prod.spline.design/j-pJlsbLv2dkeMW5/scene.splinecode" />
+
+      </div>
+
+
+    <section className={`bg-black h-fit mt-5 
       shadow-[2px_2px_19px_2px_rgba(0,0,0,0.5)]
-      flex flex-col items-center justify-center'>
+      flex flex-col items-center justify-center
+      absolute top-0 bg-transparent ${zIndex? 'z-10':'z-[-1]'}`}>
+
       <form className='text-[5vh]'>
          <div>
           <label >Full Name</label>
@@ -23,6 +50,12 @@ export const ContactForm = () => {
          <button type='submit'>Send Message</button>
 
       </form>
+
+        <div className='absolute top-10 right-6 cursor-pointer' onClick={()=>setZIndex(false)}>
+          <h1>❌</h1>
+        </div>
+
+
     </section>
   </div>
 
